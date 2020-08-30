cda3b490844147af5799187c2a4b6c9758943753
JamesChen110 <52152466+JamesChen110@users.noreply.github.com>
2020-08-30 16:25:34 +0800

    Add files via upload
    C#创建快捷方式

---
+++ shortcut.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IWshRuntimeLibrary;
+using System.Runtime.InteropServices;
+namespace WindowsFormsApp1
+{
+    class shortcut
+    {
+
+
+        string location1 = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Favorites) + "\\酷我.lnk";
+
+        string location2 = System.Environment.GetFolderPath(System.Environment.SpecialFolder.DesktopDirectory) + "\\酷我.lnk";
+
+        string location3 = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Programs) + "\\酷我.lnk";
+
+        public void appshortcut()
+        {
+
+            // Create a Windows Script Host Shell class
+           
+            IWshShell_Class shell = new IWshShell_Class();
+
+            // Define the shortcut file
+            
+            //IWshURLShortcut shortcut = shell.CreateShortcut(location1) as IWshURLShortcut;
+
+            IWshShortcut_Class shortcut = shell.CreateShortcut(location2) as IWshShortcut_Class;
+
+            
+            shortcut.WorkingDirectory = @"E:\Program Files (x86)\KuGou\KGMusic";
+            shortcut.TargetPath = @"E:\Program Files (x86)\KuGou\KGMusic\KuGou.exe";
+            // Save it
+
+            shortcut.Save();
+
+        }
+    }
+}
